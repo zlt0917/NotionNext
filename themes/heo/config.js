83e3a81d8e51b0f7e7f46c4d39a844de9c8487f4
zlt0917 <87749893+zlt0917@users.noreply.github.com>
2024-04-24 17:15:47 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -42,7 +42,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: '', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://bl.xiu.im', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
